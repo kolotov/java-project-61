2a2509cea20ddbcb1d57dea3c67a7b0837b089f0
gkdev <gkolotov.adv@gmail.com>
2024-04-13 00:39:34 +0300

    refactor: remove redundant methods

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,6 +1,10 @@
 package hexlet.code;
 
-import hexlet.code.games.*;
+import hexlet.code.games.EvenGame;
+import hexlet.code.games.CalcGame;
+import hexlet.code.games.PrimeGame;
+import hexlet.code.games.GcdGame;
+import hexlet.code.games.ProgressionGame;
 
 import java.util.Scanner;
 
@@ -16,26 +20,26 @@ public class Engine {
 
     private static final int GAME_ROUNDS = 3;
 
-    static final int TYPE_EVEN_GAME = 2;
-    static final int TYPE_CALC_GAME = 3;
-    static final int TYPE_GCD_GAME = 4;
-    static final int TYPE_PROGRESSION_GAME = 5;
-    static final int TYPE_PRIME_GAME = 6;
+    private static final int TYPE_EVEN_GAME = 2;
+    private static final int TYPE_CALC_GAME = 3;
+    private static final int TYPE_GCD_GAME = 4;
+    private static final int TYPE_PROGRESSION_GAME = 5;
+    private static final int TYPE_PRIME_GAME = 6;
 
-    static final int[] GAME_TYPES
+    private static final int[] GAME_TYPES
             = {TYPE_EVEN_GAME, TYPE_CALC_GAME, TYPE_GCD_GAME, TYPE_PROGRESSION_GAME, TYPE_PRIME_GAME};
     private static Scanner sc = new Scanner(System.in);
 
     public static void run(int gameType) {
-        print("%n" + MESSAGE_WELCOME);
+        System.out.printf("%n" + MESSAGE_WELCOME + "%n");
         var name = promptUser(MESSAGE_ASK_NAME);
-        print(MESSAGE_HELLO.formatted(name));
+        System.out.printf(MESSAGE_HELLO + "%n", name);
 
-        print(getRules(gameType));
+        System.out.println(getRules(gameType));
         startGame(gameType, name);
     }
 
-    public static void startGame(int gameType, String name) {
+    private static void startGame(int gameType, String name) {
         var wins = 0;
         while (wins < GAME_ROUNDS) {
             var questionPair = getQuestionPair(gameType);
@@ -45,34 +49,44 @@ public class Engine {
             var userAnswer = promptUser(MESSAGE_QUESTION_PROMPT.formatted(question));
             var isValid = answer.equals(userAnswer);
             if (!isValid) {
-                print(MESSAGE_GAME_OVER.formatted(userAnswer, answer, name));
+                System.out.printf(MESSAGE_GAME_OVER + "%n", userAnswer, answer, name);
                 return;
             }
 
             wins += 1;
-            print(MESSAGE_CORRECT);
+            System.out.println(MESSAGE_CORRECT);
         }
 
         if (wins == GAME_ROUNDS) {
-            print(MESSAGE_CONGRATULATION.formatted(name) + "%n");
+            System.out.printf(MESSAGE_CONGRATULATION.formatted(name) + "%n");
         }
     }
 
     public static String promptUser(String prompt) {
-        print(prompt, false);
+        System.out.print(prompt);
         return sc.nextLine();
     }
 
-    private static void print(String text) {
-        print(text, true);
+    public static String[] getMenuTitles() {
+        String[] titles = new String[GAME_TYPES.length];
+        var i = 0;
+        for (var type : GAME_TYPES) {
+            titles[i] = "%d - %s".formatted(type, getTitle(type));
+            i += 1;
+        }
+        return titles;
     }
 
-    private static void print(String text, boolean ln) {
-        var eol = ln ? "%n" : "";
-        System.out.printf(text + eol);
+    public static boolean hasGame(int gameType) {
+        for (var game : GAME_TYPES) {
+            if (game == gameType) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    static String[] getQuestionPair(int gameType) {
+    private static String[] getQuestionPair(int gameType) {
         return switch (gameType) {
             case TYPE_EVEN_GAME -> EvenGame.getQuestionPair();
             case TYPE_CALC_GAME -> CalcGame.getQuestionPair();
@@ -83,7 +97,7 @@ public class Engine {
         };
     }
 
-    static String getRules(int gameType) {
+    private static String getRules(int gameType) {
         return switch (gameType) {
             case TYPE_EVEN_GAME -> EvenGame.RULES;
             case TYPE_CALC_GAME -> CalcGame.RULES;
@@ -94,7 +108,7 @@ public class Engine {
         };
     }
 
-    static String getTitle(int gameType) {
+    private static String getTitle(int gameType) {
         return switch (gameType) {
             case TYPE_EVEN_GAME -> EvenGame.TITLE;
             case TYPE_CALC_GAME -> CalcGame.TITLE;
@@ -103,24 +117,5 @@ public class Engine {
             case TYPE_PRIME_GAME -> PrimeGame.TITLE;
             default -> throw new IllegalStateException("Unexpected value: " + gameType);
         };
-    }
-
-    static String[] getMenuTitles() {
-        String[] titles = new String[GAME_TYPES.length];
-        var i = 0;
-        for (var type : GAME_TYPES) {
-            titles[i] = "%d - %s".formatted(type, getTitle(type));
-            i += 1;
-        }
-        return titles;
-    }
-
-    static boolean hasGame(int gameType) {
-        for (var game : GAME_TYPES) {
-            if (game == gameType) {
-                return true;
-            }
-        }
-        return false;
     }
 }
